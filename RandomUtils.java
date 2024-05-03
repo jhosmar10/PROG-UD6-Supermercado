cdfce410bf2297563619b4124a6bae04f5e7888a
jhosmar10 <159432723+jhosmar10@users.noreply.github.com>
2024-05-03 18:38:46 +0100

    RandomUtils.java

---
+++ RandomUtils.java
@@ -0,0 +1,14 @@
+
+public class RandomUtils {
+
+    public static int generarNombreAleatorio() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'generarNombreAleatorio'");
+    }
+
+    public static int generarNumeroProductosAleatorio() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'generarNumeroProductosAleatorio'");
+    }
+
+}
